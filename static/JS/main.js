cb1520b5b8ddc90747e7fd45e24065abc3f1127a
Moringa School <kelvin.bett@student.moringaschool.com>
2021-06-20 09:42:52 +0300

    this will assign the varibles arguments and parameters

--- static/JS/main.js
+++ static/JS/main.js
@@ -2,7 +2,20 @@ function validate() {
     var name = document.forms["myForm"]["inputName"].value;
     var email = document.forms["myForm"]["inputEmail"].value;
     var message = document.forms["myForm"]["inputMessage"].value;
-}
+    if (name == "") {
+        alert("Name should be entered");
+        return false;
+    } else if (email = "") {
+        alert("Kindly Enter your email address");
+        return false;
+    } else if (message == "") {
+        alert("Ensure to provide your feedback response");
+        return false;
+    } else {
+        alert(name + ", Thankyou for reaching out to us. Your response is highly appreciated");
+    }
+
+};
 $(document).ready(function() {
     $("div.design").click(function() {
         $("img#design").toggle();
